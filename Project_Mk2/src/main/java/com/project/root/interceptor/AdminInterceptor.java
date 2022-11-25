9564a2bd75c2d8943c22211501f792e326b82c59
smLee035117 <lee970908@naver.com>
2022-11-25 16:49:37 +0900

    interceptor 기능 구현, navigator 부분 적용.
    interceptor 구현부분, sevlet-context.xml
    
    memberMapper 에서 로그인한 유저의 memKey값 가져오는 부분 추가.
    controller에서 로그인시 가져온 값 세션에 추가
    
    세션에 추가된 값으로 interceptor에서 확인함.
    tables, tables 에 navigator 부분 c import 로 적용

---
+++ Project_Mk2/src/main/java/com/project/root/interceptor/AdminInterceptor.java
@@ -0,0 +1,29 @@
+package com.project.root.interceptor;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AdminInterceptor implements HandlerInterceptor  {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		
+		HttpSession session = request.getSession();
+		String checkKey = (String)session.getAttribute("mem_key");
+		if(!checkKey.equals("A")) {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>"+
+			"alert('접근 권한이 없습니다.');"
+			+"location.href='"+ request.getContextPath()+"/'</script>");
+			return false;
+		}
+		return true;
+	}
+	
+}
